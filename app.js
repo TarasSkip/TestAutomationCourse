5253341664d9f1d92aef5a95770a1f280258c4e8
Taras Skip <Taras.Skip@eleks.com>
2018-11-14 17:49:41 +0200

    1st change

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 console.log("\nNice work! Your first application have been launched.");
 console.log("Now, I need to know your name, type it below: ");
 
+//SOME COMMENT 
+
 let stdin = process.openStdin();
 
 stdin.addListener("data", (txt) => {
